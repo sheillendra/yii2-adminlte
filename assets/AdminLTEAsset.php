d8a7d2c73f954169d2680aff7b087cf1eb8f72c6
sheillendra <sheillendra@yahoo.com>
2016-02-22 07:47:31 +0700

    init

--- assets/AdminLTEAsset.php
+++ assets/AdminLTEAsset.php
@@ -9,16 +9,17 @@ use yii\web\AssetBundle;
 
 class AdminLTEAsset extends AssetBundle {
     
-    public $sourcePath = '@almasaeed2010/adminlte/dist';
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/dist';
     public $css = [
         'css/AdminLTE.min.css',
-        'css/skins/_all.css'
+        'css/skins/_all-skins.css'
     ];
     public $js = [
         'js/app.js',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
         'sheillendra\theme\assets\FontAwesomeAsset'
     ];
     public $publishOptions=['forceCopy'=>YII_DEBUG];
