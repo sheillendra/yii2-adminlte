c1f2b09a5f411f0b447b1bbe45e2de40cf3c7db9
sheillendra <sheillendra@yahoo.com>
2016-03-15 05:29:00 +0700

    init

---
+++ assets/IoniconsAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @link http://www.sheillendra.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace sheillendra\adminlte\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Sheillendra <sheillendra@yahoo.com>
+ * @since 2.0
+ */
+class IoniconsAsset extends AssetBundle {
+
+    public $sourcePath = '@bower/ionicons';
+    public $css = [
+        'css/ionicons.min.css',
+    ];
+    public $js = [];
+    public $depends = [];
+}
